ff758f67f2d601bd43f4f586362175b0b262b6af
paolok1 <gpaolok1@gmail.com>
2024-09-10 13:37:57 +0200

    selfwork_operatori_3_completato

---
+++ main.js
@@ -0,0 +1,19 @@
+let num1 = 15;
+let num2 = 5;
+let add = num1 + num2;
+console.log(add);
+let sottr = num1-num2;
+console.log(sottr);
+let moltip = num1 * num2;
+console.log(moltip);
+let divis = num1 / num2;
+console.log(divis);
+let pz0 = num1 ** 2;
+console.log(pz0);
+let pz1 = num2 ** 2;
+console.log(pz1);
+console.log( "Con i numeri scelti, i risultati delle varie operazioni sono : somma (20) , sottrazione (10) , moltiplicazione (75) , divisione (3) , potenza (225 , 25)" );
+
+
+
+
